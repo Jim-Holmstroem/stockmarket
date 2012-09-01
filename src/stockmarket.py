caa8a38a522194bc6b0f61aebd6d65276d7751fd
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-01 14:09:35 +0200

    started fixing the database issues

--- src/stockmarket.py
+++ src/stockmarket.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import urllib2
 import json
 import sqlite3 as sql
@@ -33,6 +35,7 @@ class Portfolio(object):
             raise e
         finally:
             if con:
+                con.commit()
                 con.close()
 
     def get_cash(self):
@@ -72,26 +75,29 @@ class Portfolio(object):
             
             int(amount) #making sure
 
-            cur.execute("SELECT count(*) FROM stocks WHERE token=?;")
+            cur.execute("SELECT count(*) FROM stocks WHERE token=?;",(token,))
             if(cur.fetchone()[0]==0): #check if exists already, if not create it
-                cur.execute("INSERT INTO stocks VALUES ( ? , 0 );",(token,0))
-
-            aprice = Stockmarket.lookup([token,]) #check price
+                cur.execute("INSERT INTO stocks VALUES ( ? , ? );",(token,0))
+            
+            aprice = Stockmarket.lookup([token,])[0] #check price
             cur.execute("UPDATE cash SET money = ( money - ({withdraw}) );".format(withdraw=aprice*amount)) #draw cash
-            cur.execute("UPDATE stocks SET amount = ( amount + ({damount}) ) WHERE token = ?;".format(damount="%+d"%amount),(abs(amount),token)) #get the stock
-        
+            cur.execute("UPDATE stocks SET amount = ( amount + ({damount}) ) WHERE token = ?;".format(damount=amount),(token,)) #get the stock
+            
         except sql.Error as e:
             raise e
         finally:
             if con:
+                con.commit()
                 con.close()
 
     def current_value(self):
-        stocks = zip(*self.get_stocks())
-        return self.cash + sum(starmap(operator.mul, izip( stocks[1], map(Stockmarket.lookup, stocks[0]))))
+        value = self.get_cash()
+        stocks = zip(*self.get_stocks()) #transpose the stocks
+        if(stocks):
+            value += sum(starmap(operator.mul, izip( stocks[1], map(Stockmarket.lookup, stocks[0]))))
+        return value
 
 class Stockmarket(object):
-
     @staticmethod
     def lookup(symbols):
         yql = "select LastTradePriceOnly from yahoo.finance.quotes where symbol in ('{params}')".format(params="','".join(symbols)) 
@@ -100,24 +106,33 @@ class Stockmarket(object):
 
         try: 
             result = urllib2.urlopen(url)
-        except urllib2.HTTPError, e:
-            print ("HTTP error: ", e.code)
-        except urllib2.URLError, e:
-            print ("Network error: ", e.reason)
+            rawdata = result.read()
+            data = json.loads( rawdata )
+            jsonQuotes = data['query']['results']['quote']
+            
+            pythonQuotes = []
+            if type( jsonQuotes ) == type ( dict() ):
+                pythonQuotes.append( jsonQuotes )
+            else:
+                pythonQuotes = jsonQuotes
            
-        data = json.loads( result.read() )
-        jsonQuotes = data['query']['results']['quote']
+            pythonQuotes = map(lambda x: float(x['LastTradePriceOnly']), pythonQuotes)
+            return pythonQuotes
         
-        pythonQuotes = []
-        if type( jsonQuotes ) == type ( dict() ):
-            pythonQuotes.append( jsonQuotes )
-        else:
-            pythonQuotes = jsonQuotes
-       
-        pythonQuotes = map(lambda x: float(x['LastTradePriceOnly']), pythonQuotes)
-        return pythonQuotes
+        except urllib2.HTTPError, e:
+            print("HTTP error: ", e.code)
+            raise e
+        except urllib2.URLError, e:
+            print("Network error: ", e.reason)
+            raise e
+        except Exception, e:
+            print("lookup failed", e)
+            raise e
 
+        
 p = Portfolio("sofia")
-print p.get_stocks()
-print p.current_value()
-
+print("stocks =",p.get_stocks())
+print(p.current_value())
+p.update_stock("GOOG",2)
+print("stocks =",p.get_stocks())
+print(p.current_value())
